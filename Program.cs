6ed067c89845427a39b66a2ea6d947a80aaef26f
Nadezhda <nbykova79@gmail.com>
2022-08-14 18:38:23 +0300

    Создали метод заполнения нового массива элементами исходного массива с длинной не более 3

--- Program.cs
+++ Program.cs
@@ -23,3 +23,19 @@ int CountWordArray(string []array)
     }
     return count;
 }
+string[] SelectMemberArray(string [] array, int count)
+{
+    string [] NewArray = new string [count];
+    int j=0;
+    for (int i=0; i<N; i++)
+    {
+        if (array[i].Length<=3)
+        {
+            NewArray[j]=array[i];
+            j++;
+        }
+    }
+    return NewArray;
+}
+string [] NewArray = EnterArray();
+Console.WriteLine(string.Join("|",SelectMemberArray(NewArray,CountWordArray(NewArray))));
